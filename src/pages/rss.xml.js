119e2f219cebfa85e7f59483e9850a07d71d5728
Stephen Harwell <steveharwell@gmail.com>
2023-09-10 10:09:50 -0500

    upgraded rss.xml to astro 3

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function get() {
-  return rss({
+export async function GET() {
+  return new Response((await rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: 'https://stephenharwell.dev',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: await pagesGlobToRssItems(import.meta.glob('./projects/**/*.md')),
     customData: `<language>en-us</language>`,
-  });
+  })).body);
 }
